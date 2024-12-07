f561de1ca8e88c9035b73ed02125f8d92926fbc1
Cserpák Róbert <robi.cserpak@gmail.com>
2024-12-07 11:22:53 +0100

    Day 7 faster concat

--- Day7.hs
+++ Day7.hs
@@ -2,17 +2,17 @@ module Day7 () where
 
 main :: IO ()
 main = do
-    let fileName = "inputs/7.txt"
-    file <- readFile fileName
-    let input = map (break (==':')) $ lines file
-    let parsed = parseInput input
+  let fileName = "inputs/7.txt"
+  file <- readFile fileName
+  let input = map (break (== ':')) $ lines file
+  let parsed = parseInput input
 
-    print $ "firstPart: "  ++ show (firstPart parsed)
-    print $ "secondPart: " ++ show (secondPart parsed)
+  print $ "firstPart: " ++ show (firstPart parsed)
+  print $ "secondPart: " ++ show (secondPart parsed)
 
 parseInput :: [([Char], [Char])] -> [(Int, [Int])]
-parseInput ((a,bs):xs) = (read a, map read . words $ tail bs) : parseInput xs
-parseInput _ = []
+parseInput ((a, bs) : xs)   = (read a, map read . words $ tail bs) : parseInput xs
+parseInput _                = []
 
 firstPart :: [(Int, [Int])] -> Int
 firstPart = sum . map fst . filter fpValid
@@ -20,12 +20,12 @@ firstPart = sum . map fst . filter fpValid
 secondPart :: [(Int, [Int])] -> Int
 secondPart = sum . map fst . filter spValid
 
-fpValid:: (Int,[Int]) -> Bool
-fpValid (y,x1:x2:xs) = fpValid (y,x1+x2:xs) || fpValid (y,x1*x2:xs)
-fpValid (y,[x]) = y == x
-fpValid _ = False
+fpValid :: (Int, [Int]) -> Bool
+fpValid (y, x1 : x2 : xs)   = fpValid (y, x1 + x2 : xs) || fpValid (y, x1 * x2 : xs)
+fpValid (y, [x])            = y == x
+fpValid _                   = False
 
-spValid:: (Int,[Int]) -> Bool
-spValid (y,x1:x2:xs) = spValid (y,x1+x2:xs) || spValid (y,x1*x2:xs) || spValid (y,read (show x1 ++ show x2):xs)
-spValid (y,[x]) = y == x
-spValid _ = False
+spValid :: (Int, [Int]) -> Bool
+spValid (y, x1 : x2 : xs)   = spValid (y, x1 + x2 : xs) || spValid (y, x1 * x2 : xs) || spValid (y, (x1 * 10 ^ length (show x2) + x2) : xs)
+spValid (y, [x])            = y == x
+spValid _                   = False
